cb3d0278b55512b66599ca2fd1e4fd6aa409691d
LucasRochaAbraao <lucasrabraao@gmail.com>
2021-02-24 00:47:10 -0300

    initial commit with db working

---
+++ models.py
@@ -0,0 +1,39 @@
+# pylint: disable=no-name-in-module
+import os
+from pymongo import MongoClient
+from pydantic import BaseModel, Field
+from typing import Optional
+from bson import ObjectId
+
+cluster = MongoClient(os.environ["DB_URI"])
+db = cluster['vagas_quick']
+
+class PyObjectId(ObjectId):
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError('Invalid objectid')
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type='string')
+
+
+class Vaga(BaseModel):
+    id: Optional[PyObjectId] = Field(alias='_id')
+    vaga: str
+    atividades: str
+    requisitos: str
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: str
+        }
+
---
+++ vagas.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+from models import db, Vaga
+
+app = FastAPI()
+
+@app.get('/vagas')
+async def todas_vagas():
+    vagas = []
+    for vaga in db.vagas.find():
+        vagas.append(Vaga(**vaga))
+   
+    return {'vagas': vagas}
+
+
+@app.post('/vagas')
+async def criar_vaga(vaga: Vaga):
+    if hasattr(vaga, 'id'):
+        delattr(vaga, 'id')
+    ret = db.vagas.insert_one(vaga.dict(by_alias=True))
+    vaga.id = ret.inserted_id
+
+    return {'vaga': vaga}
